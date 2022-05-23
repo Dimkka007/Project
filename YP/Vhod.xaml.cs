5e2946c3a1b95f5b4a37cd5820004ca9c38f016e
Dimkka007 <yddjfdr@gmail.com>
2022-05-24 01:35:00 +0500

    Version1

---
+++ YP/Vhod.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace YP
+{
+    /// <summary>
+    /// Логика взаимодействия для Vhod.xaml
+    /// </summary>
+    public partial class Vhod : Page
+    {
+        
+        public Vhod()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void EnterVhod(object sender, RoutedEventArgs e)
+        {
+            YPEntities dimka = new YPEntities();
+            Vhod autohorization = new Vhod();
+            var user = dimka.Agent.FirstOrDefault(x => x.Email == Email.Text && x.Password == Password.Text);
+            if(user == null)
+            {
+                MessageBox.Show("Пароль не верный");
+
+            }
+            else
+            {
+                Manager.MainFrame.Navigate(new Vse());
+                MessageBox.Show("Здравствуйте! " + user.Title);
+            }
+        }
+    }
+}
---
+++ YP/Vse.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YP
+{
+    /// <summary>
+    /// Логика взаимодействия для Vse.xaml
+    /// </summary>
+    public partial class Vse : Page
+    {
+        public Vse()
+        {
+            InitializeComponent();
+            Product.ItemsSource = YPEntities.GetContext().Product.ToList();
+            
+        }
+
+        private void SortBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            if(SortBox.SelectedIndex == 0)
+            {
+                Product.ItemsSource = YPEntities.GetContext().Product.OrderBy(x => x.MinCostForAgent).ToList();
+            }
+            if (SortBox.SelectedIndex == 1)
+            {
+                Product.ItemsSource = YPEntities.GetContext().Product.OrderByDescending(x => x.MinCostForAgent).ToList();
+            }
+        }
+
+        private void FiltBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ProductType productType = new ProductType();
+            if (FiltBox.SelectedIndex == 0)
+            {
+                Product.ItemsSource = YPEntities.GetContext().ProductType.OfType<ProductType>().Where(r=>r.Title.Contains("Запаска")).ToList();
+            }
+            if (FiltBox.SelectedIndex == 1)
+            {
+                Product.ItemsSource = YPEntities.GetContext().Product.OrderBy(x => x.MinCostForAgent).ToList();
+            }
+            if (FiltBox.SelectedIndex == 2)
+            {
+                Product.ItemsSource = YPEntities.GetContext().Product.OrderBy(x => x.MinCostForAgent).ToList();
+            }
+            if (FiltBox.SelectedIndex == 3)
+            {
+                Product.ItemsSource = YPEntities.GetContext().Product.OrderBy(x => x.MinCostForAgent).ToList();
+            }
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
